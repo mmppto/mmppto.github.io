9111576e0063cc21c20321c27e974204e702a747
mmppto <mmppto@outlook.com>
2022-05-18 18:11:32 +0800

    Site updated: 2022-05-18 18:11:31

--- js/column.js
+++ js/column.js
@@ -1,12 +1,1 @@
-(function() {
-    function $() {
-        return Array.prototype.slice.call(document.querySelectorAll.apply(document, arguments));
-    }
-
-    // copy widgets in the right column, when exist, to the bottom of the left column
-    if ($('.columns .column-right').length && $('.columns .column-right-shadow').length && !$('.columns .column-right-shadow')[0].children.length) {
-        for (const child of $('.columns .column-right')[0].children) {
-            $('.columns .column-right-shadow')[0].append(child.cloneNode(true));
-        }
-    }
-}());
+"use strict";function _createForOfIteratorHelper(r,t){var e,n="undefined"!=typeof Symbol&&r[Symbol.iterator]||r["@@iterator"];if(!n){if(Array.isArray(r)||(n=_unsupportedIterableToArray(r))||t&&r&&"number"==typeof r.length)return n&&(r=n),e=0,{s:t=function(){},n:function(){return e>=r.length?{done:!0}:{done:!1,value:r[e++]}},e:function(r){throw r},f:t};throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.")}var o,a=!0,l=!1;return{s:function(){n=n.call(r)},n:function(){var r=n.next();return a=r.done,r},e:function(r){l=!0,o=r},f:function(){try{a||null==n.return||n.return()}finally{if(l)throw o}}}}function _unsupportedIterableToArray(r,t){if(r){if("string"==typeof r)return _arrayLikeToArray(r,t);var e=Object.prototype.toString.call(r).slice(8,-1);return"Map"===(e="Object"===e&&r.constructor?r.constructor.name:e)||"Set"===e?Array.from(r):"Arguments"===e||/^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(e)?_arrayLikeToArray(r,t):void 0}}function _arrayLikeToArray(r,t){(null==t||t>r.length)&&(t=r.length);for(var e=0,n=new Array(t);e<t;e++)n[e]=r[e];return n}!function(){function r(){return Array.prototype.slice.call(document.querySelectorAll.apply(document,arguments))}if(r(".columns .column-right").length&&r(".columns .column-right-shadow").length&&!r(".columns .column-right-shadow")[0].children.length){var t,e=_createForOfIteratorHelper(r(".columns .column-right")[0].children);try{for(e.s();!(t=e.n()).done;){var n=t.value;r(".columns .column-right-shadow")[0].append(n.cloneNode(!0))}}catch(r){e.e(r)}finally{e.f()}}}();
